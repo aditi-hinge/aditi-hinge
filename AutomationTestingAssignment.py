7c1761a36dc1d4202f18bec28b3fdd7d65488bd3
aditi-hinge <95167364+aditi-hinge@users.noreply.github.com>
2021-11-28 19:27:13 +0900

    Update AutomationTestingAssignment.py

--- AutomationTestingAssignment.py
+++ AutomationTestingAssignment.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 
 driver= webdriver.Edge("C://Users//Aditi//Desktop//msedgedriver.exe")
+
 driver.get("https://hotel.testplanisphere.dev/ja/login.html")
 driver.find_element_by_id("email").send_keys("blue@gmail.com")
 driver.find_element_by_id("password").send_keys("applebanana")
